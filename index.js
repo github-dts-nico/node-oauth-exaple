b7dcd8885f3094844e5f31509b32aeb49829fb7b
Gusti Rachman Nico <nicko.punya.email@gmail.com>
2020-10-14 11:42:58 +0700

    commit init

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // Import the express lirbary
-const express = require('express')
+const express = require("express");
 
 // Import the axios library, to make HTTP requests
-const axios = require('axios')
+const axios = require("axios");
 
 // This is the client ID and client secret that you obtained
 // while registering the application
-const clientID = '<your client id>'
-const clientSecret = '<your client secret>'
+const clientID = "0efb672856192d103afd";
+const clientSecret = "b07f6a485d83f3e57f25ea50f725907314146620";
 
 // Create a new express application and use
 // the express static middleware, to serve all files
 // inside the public directory
-const app = express()
-app.use(express.static(__dirname + '/public'))
+const app = express();
+app.use(express.static(__dirname + "/public"));
 
-app.get('/oauth/redirect', (req, res) => {
+app.get("/oauth/redirect", (req, res) => {
   // The req.query object has the query params that
   // were sent to this route. We want the `code` param
-  const requestToken = req.query.code
+  const requestToken = req.query.code;
   axios({
     // make a POST request
-    method: 'post',
+    method: "post",
     // to the Github authentication API, with the client ID, client secret
     // and request token
     url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
     // Set the content type header, so that we get the response in JSOn
     headers: {
-      accept: 'application/json'
-    }
+      accept: "application/json",
+    },
   }).then((response) => {
     // Once we get the response, extract the access token from
     // the response body
-    const accessToken = response.data.access_token
+    const accessToken = response.data.access_token;
     // redirect the user to the welcome page, along with the access token
-    res.redirect(`/welcome.html?access_token=${accessToken}`)
-  })
-})
+    res.redirect(`/welcome.html?access_token=${accessToken}`);
+  });
+});
 
 // Start the server on port 8080
-app.listen(8080)
+app.listen(8081);
